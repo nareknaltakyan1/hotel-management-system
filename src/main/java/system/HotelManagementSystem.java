41c824b4db2c48a1b2b2d72140220d5f8ed98c14
Naltakyan <narek.naltakyan@dataart.com>
2022-05-14 17:18:09 +0400

    Initial commit

---
+++ src/main/java/system/HotelManagementSystem.java
@@ -0,0 +1,11 @@
+package system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystem {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementSystem.class, args);
+    }
+}
