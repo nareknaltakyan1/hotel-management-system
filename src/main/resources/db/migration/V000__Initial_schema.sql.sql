53c262242008c5efda1b761368423584977273b0
ArmanNaltakyan <92972034+ArmanNaltakyan@users.noreply.github.com>
2022-05-15 12:58:42 +0400

    change sql initial script

--- src/main/resources/db/migration/V000__Initial_schema.sql.sql
+++ src/main/resources/db/migration/V000__Initial_schema.sql.sql
@@ -12,4 +12,24 @@ create table contact
     primary key(id)
 );
 
+create table room(
+    id bigserial not null;
+    type varchar(255) not null;
+    primary key(id)
+)
+
+create table guest(
+    id bigserial not null;
+    passport varchar(255) not null;
+    name varchar(255) not null;
+    phone varchar(255) not null;
+    email varchar(255);
+    primary key(id)
+)
+
+create table employee(
+    id bigserial not null ;
+    role varchar(255) not null;
+)
+
 create index idx_contact_name on contact (name);
