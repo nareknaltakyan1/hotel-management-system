1ff223fb38f7dd3e337ad5de85835b22cbe7c78f
Naltakyan <nareknaltakyan1@gmail.com>
2022-05-29 18:06:17 +0400

    formatting

--- src/main/java/com/naltakyan/hotelmanagement/model/Employee.java
+++ src/main/java/com/naltakyan/hotelmanagement/model/Employee.java
@@ -4,12 +4,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.springframework.validation.annotation.Validated;
 
 @Entity
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
@@ -17,21 +15,21 @@ import org.springframework.validation.annotation.Validated;
 @Setter
 public class Employee extends TimeStampedDomainEntity {
 
-    @Enumerated(EnumType.STRING)
-    private EmployeeRole role;
+  @Enumerated(EnumType.STRING)
+  private EmployeeRole role;
 
-    private String passport;
+  private String passport;
 
-    @Column(name = "first_name")
-    private String firstName;
+  @Column(name = "first_name")
+  private String firstName;
 
-    @Column(name = "last_name")
-    private String lastName;
+  @Column(name = "last_name")
+  private String lastName;
 
-    @Column(name = "profile_image_url")
-    private String profileImageUrl;
+  @Column(name = "profile_image_url")
+  private String profileImageUrl;
 
-    private String phone;
+  private String phone;
 
-    private String email;
+  private String email;
 }
