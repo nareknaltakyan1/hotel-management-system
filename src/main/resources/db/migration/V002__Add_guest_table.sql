1a1e5b0eb31f7f1834a88e9a91371197d0620e83
Leana0 <KpuytAchqerUres?Hn^^)>
2022-05-29 17:23:32 +0400

    add comments

--- src/main/resources/db/migration/V002__Add_guest_table.sql
+++ src/main/resources/db/migration/V002__Add_guest_table.sql
@@ -4,23 +4,14 @@ create table guest
     first_name varchar(255)          NOT NULL,
     last_name  varchar(255),
     passport   varchar(255) UNIQUE   NOT NULL,
+    phone      varchar(255) UNIQUE   NOT NULL,
+    email      varchar(255) UNIQUE   NOT NULL,
     created    timestamp             NOT NULL,
     deleted    timestamp,
-    phone      varchar(255) UNIQUE   NOT NULL,
-    email      varchar(255) UNIQUE   NOT NULL
-)
+    updated    timestamp
+);
 
 
-CREATE INDEX idx_guest_passport ON guest
-    (
-     passport
-        );
-
-CREATE INDEX idx_guest_email ON guest
-    (
-     email
-        );
-CREATE INDEX idx_guest_phone ON guest
-    (
-     phone
-        );
+CREATE INDEX idx_guest_passport ON guest (passport);
+CREATE INDEX idx_guest_email ON guest (email);
+CREATE INDEX idx_guest_phone ON guest (phone);
