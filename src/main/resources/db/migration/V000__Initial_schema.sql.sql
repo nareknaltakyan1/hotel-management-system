7c30c6946b3053d41cf9c5c0df6542939480d526
Naltakyan <nareknaltakyan1@gmail.com>
2022-05-29 13:57:23 +0400

    sql

--- src/main/resources/db/migration/V000__Initial_schema.sql.sql
+++ src/main/resources/db/migration/V000__Initial_schema.sql.sql
@@ -1,50 +1,5 @@
-create table room
-(
-    id      bigserial    not null,
-    type    varchar(255) not null,
-    created timestamp    not null,
-    updated timestamp    not null,
-    deleted timestamp,
-    primary key (id)
-);
-
-create index idx_room_type on room (type);
-
-create table guest
-(
-    id       bigserial    not null,
-    passport varchar(255) not null UNIQUE,
-    name     varchar(255) not null,
-    phone    varchar(255) not null UNIQUE,
-    email    varchar(255),
-    created  timestamp    not null,
-    updated  timestamp    not null,
-    deleted  timestamp,
-    primary key (id)
-);
-
-create index idx_guest_email on guest (email);
-create index idx_guest_name on guest (name);
-
-create table employee
-(
-    id       bigserial    not null,
-    role     varchar(255) not null,
-    passport varchar(255) not null UNIQUE,
-    name     varchar(255) not null,
-    phone    varchar(255) not null UNIQUE,
-    email    varchar(255),
-    created  timestamp    not null,
-    updated  timestamp    not null,
-    deleted  timestamp,
-    primary key (id)
-);
-
-create index idx_employee_email on employee (email);
-create index idx_employee_name on employee (name);
-create index idx_employee_role on employee (role);
-
-
+-- create database hotel_management_system;
+-- -- ************************************** worker_type
 
 CREATE TABLE worker_type
 (
@@ -53,7 +8,34 @@ CREATE TABLE worker_type
     CONSTRAINT PK_21 PRIMARY KEY ("id")
 );
 
+-- ************************************** reservation_status
 
+CREATE TABLE reservation_status
+(
+    "id"   bigserial   NOT NULL,
+    status varchar(50) NOT NULL,
+    CONSTRAINT PK_49 PRIMARY KEY ("id")
+);
+
+-- ************************************** room_type
+
+CREATE TABLE room_type
+(
+    "id" bigserial    NOT NULL,
+    type varchar(255) NOT NULL,
+    CONSTRAINT PK_10 PRIMARY KEY ("id")
+);
+-- ************************************** additional_service
+
+CREATE TABLE additional_service
+(
+    "id"  bigserial    NOT NULL,
+    name  varchar(255) NOT NULL,
+    price money        NOT NULL,
+    CONSTRAINT PK_42 PRIMARY KEY ("id")
+);
+
+-- ************************************** guest
 
 CREATE TABLE guest
 (
@@ -63,66 +45,12 @@ CREATE TABLE guest
     passport   varchar(255) NULL,
     created    date         NOT NULL,
     deleted    date         NULL,
+    phone      varchar(255) NOT NULL,
+    email      varchar(255),
     CONSTRAINT PK_34 PRIMARY KEY ("id")
 );
 
-
-
-CREATE TABLE reservation
-(
-    "id"        bigserial NOT NULL,
-    from_date timestamp NOT NULL,
-    status_id bigserial NOT NULL,
-    guest_id  bigserial NOT NULL,
-    to_date   timestamp NULL,
-    CONSTRAINT PK_53 PRIMARY KEY ( "id" ),
-    CONSTRAINT FK_56 FOREIGN KEY ( guest_id ) REFERENCES guest ( "id" ),
-    CONSTRAINT FK_59 FOREIGN KEY ( status_id ) REFERENCES reservation_status ( "id" )
-);
-
-CREATE INDEX FK_58 ON reservation
-    (
-     guest_id
-        );
-
-CREATE INDEX FK_61 ON reservation
-    (
-     status_id
-        );
-
-
-
-
-CREATE TABLE reservation_additional_services
-(
-    "id"           bigserial NOT NULL,
-    "count"        int       NOT NULL,
-    service_id     bigserial NOT NULL,
-    reservation_id bigserial NOT NULL,
-    CONSTRAINT PK_67 PRIMARY KEY ("id"),
-    CONSTRAINT FK_69 FOREIGN KEY (reservation_id) REFERENCES reservation ("id"),
-    CONSTRAINT FK_72 FOREIGN KEY (service_id) REFERENCES additional_service ("id")
-);
-
-CREATE INDEX FK_71 ON reservation_additional_services
-    (
-     reservation_id
-    );
-
-CREATE INDEX FK_74 ON reservation_additional_services
-    (
-     service_id
-    );
-
-
-CREATE TABLE reservation_status
-(
-    "id"   bigserial   NOT NULL,
-    status varchar(50) NOT NULL,
-    CONSTRAINT PK_49 PRIMARY KEY ("id")
-);
-
-
+-- ************************************** room
 
 CREATE TABLE room
 (
@@ -138,47 +66,84 @@ CREATE TABLE room
 CREATE INDEX FK_15 ON room
     (
      room_type_id
-    );
+        );
 
-CREATE TABLE room_type
+-- ************************************** reservation
+
+CREATE TABLE reservation
 (
-    "id" bigserial    NOT NULL,
-    type varchar(255) NOT NULL,
-    CONSTRAINT PK_10 PRIMARY KEY ("id")
+    "id"      bigserial NOT NULL,
+    from_date timestamp NOT NULL,
+    room_id   bigserial NOT NULL,
+    status_id bigserial NOT NULL,
+    guest_id  bigserial NOT NULL,
+    to_date   timestamp NULL,
+    price     money     NOT NULL,
+    created   timestamp NOT NULL,
+    CONSTRAINT PK_53 PRIMARY KEY ("id"),
+    CONSTRAINT FK_56 FOREIGN KEY (guest_id) REFERENCES guest ("id"),
+    CONSTRAINT FK_59 FOREIGN KEY (status_id) REFERENCES reservation_status ("id"),
+    CONSTRAINT FK_80 FOREIGN KEY (room_id) REFERENCES room ("id")
 );
 
+CREATE INDEX FK_58 ON reservation
+    (
+     guest_id
+        );
+
+CREATE INDEX FK_61 ON reservation
+    (
+     status_id
+        );
+
+CREATE INDEX FK_82 ON reservation
+    (
+     room_id
+        );
+
+-- ************************************** reservation_additional_services
+
+CREATE TABLE reservation_additional_services
+(
+    "id"           bigserial NOT NULL,
+    "count"        int       NOT NULL,
+    service_id     bigserial NOT NULL,
+    reservation_id bigserial NOT NULL,
+    CONSTRAINT PK_67 PRIMARY KEY ("id"),
+    CONSTRAINT FK_69 FOREIGN KEY (reservation_id) REFERENCES reservation ("id"),
+    CONSTRAINT FK_72 FOREIGN KEY (service_id) REFERENCES additional_service ("id")
+);
+
+CREATE INDEX FK_71 ON reservation_additional_services
+    (
+     reservation_id
+        );
+
+CREATE INDEX FK_74 ON reservation_additional_services
+    (
+     service_id
+        );
+
+
+-- ************************************** worker
 
 CREATE TABLE worker
 (
-    "id"                 NOT NULL,
-    worker_type_id    bigserial NOT NULL,
+    "id"              bigserial    NOT NULL,
+    worker_type_id    bigserial    NOT NULL,
     first_name        varchar(255) NOT NULL,
     last_name         varchar(255) NOT NULL,
     passport          varchar(255) NOT NULL,
     profile_image_url varchar(255) NULL,
-    created           date NOT NULL,
-    deleted           date NULL,
-    CONSTRAINT PK_18 PRIMARY KEY ( "id" ),
-    CONSTRAINT FK_23 FOREIGN KEY ( worker_type_id ) REFERENCES worker_type ( "id" )
+    created           date         NOT NULL,
+    deleted           date         NULL,
+    phone             varchar(255) NOT NULL,
+    email             varchar(255) NOT NULL,
+    CONSTRAINT PK_18 PRIMARY KEY ("id"),
+    CONSTRAINT FK_23 FOREIGN KEY (worker_type_id) REFERENCES worker_type ("id")
 );
 
 CREATE INDEX FK_25 ON worker
     (
      worker_type_id
         );
-
-
-CREATE TABLE worker_type
-(
-    "id" bigserial    NOT NULL,
-    type varchar(255) NOT NULL,
-    CONSTRAINT PK_21 PRIMARY KEY ("id")
-);
-
-
-
-
-
-
-
-
