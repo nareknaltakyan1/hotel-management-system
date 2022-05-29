51edd5a0bd2998c5f60de4f467fe42595f8c2076
Leana0 <KpuytAchqerUres?Hn^^)>
2022-05-29 16:53:04 +0400

    Add your comment guest

--- src/main/resources/db/migration/V002__Initial_schema.sql
+++ src/main/resources/db/migration/V002__Initial_schema.sql
@@ -1,10 +1,24 @@
-create  table  guest (
-    guest_id bigserial primary  key Not null ,
-    first_name varchar(255) not null ,
-    last_name varchar(255),
-    passport varchar(255),
-    created  timestamp not null ,
-    deleted timestamp ,
-    phone varchar(255) not null ,
-    email varchar(255)
+create table guest
+(
+    id   bigserial PRIMARY KEY NOT NULL,
+    first_name varchar(255)          not null,
+    last_name  varchar(255),
+    passport   varchar(255),
+    created    timestamp             not null,
+    deleted    timestamp,
+    phone      varchar(255)          not null,
+    email      varchar(255)
 )
+
+
+ CREATE INDEX FK_1 ON guest
+     (
+      passport
+         );
+
+
+
+SELECT
+    DISTINCT passport
+FROM
+    guest;
