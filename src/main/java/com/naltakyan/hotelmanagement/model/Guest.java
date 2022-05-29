7c35e758a393d42432402059ccb2e230707aaf38
Naltakyan <nareknaltakyan1@gmail.com>
2022-05-29 18:04:51 +0400

    formatting

--- src/main/java/com/naltakyan/hotelmanagement/model/Guest.java
+++ src/main/java/com/naltakyan/hotelmanagement/model/Guest.java
@@ -1,34 +1,28 @@
 package com.naltakyan.hotelmanagement.model;
 
 import javax.persistence.*;
-
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-
-
-
 @Entity
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @Getter
 @Setter
-public class Guest extends  TimeStampedDomainEntity {
+public class Guest extends TimeStampedDomainEntity {
 
+  @Column(nullable = false, name = "first_name")
+  private String firstName;
 
-    @Column(nullable = false,name = "first_name")
-    private String firstName;
+  @Column(name = "last_name")
+  private String lastName;
 
-    @Column(name = "last_name")
-    private String lastName;
+  @Column(nullable = false)
+  private String passport;
 
-    @Column(nullable = false)
-    private String passport;
+  @Column(nullable = false)
+  private String phone;
 
-    @Column(nullable = false)
-    private String phone;
-
-    private String email;
-
+  private String email;
 }
--- src/main/java/com/naltakyan/hotelmanagement/repository/GuestRepository.java
+++ src/main/java/com/naltakyan/hotelmanagement/repository/GuestRepository.java
@@ -4,9 +4,5 @@ import com.naltakyan.hotelmanagement.model.Guest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface GuestRepository extends JpaRepository<Long, Guest> {
-
-
-}
+public interface GuestRepository extends JpaRepository<Long, Guest> {}
