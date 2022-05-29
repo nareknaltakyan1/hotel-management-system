f8538f5e021e79e4a86c14d5e96b50b1b568cace
Leana0 <KpuytAchqerUres?Hn^^)>
2022-05-29 17:19:04 +0400

    add comments

--- src/main/resources/db/migration/V002__Add_guest_table.sql
+++ src/main/resources/db/migration/V002__Add_guest_table.sql
@@ -3,11 +3,11 @@ create table guest
     id         bigserial PRIMARY KEY NOT NULL,
     first_name varchar(255)          NOT NULL,
     last_name  varchar(255),
-    passport   varchar(255) UNIQUE,
+    passport   varchar(255) UNIQUE   NOT NULL,
     created    timestamp             NOT NULL,
     deleted    timestamp,
     phone      varchar(255) UNIQUE   NOT NULL,
-    email      varchar(255) UNIQUE
+    email      varchar(255) UNIQUE   NOT NULL
 )
 
 
