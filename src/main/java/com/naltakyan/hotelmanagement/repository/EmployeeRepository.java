57f5fe2f4061575c88ba09bc51ad1d4ce7836718
ArmanNaltakyan <92972034+ArmanNaltakyan@users.noreply.github.com>
2022-05-29 17:56:27 +0400

    add repository annotation

--- src/main/java/com/naltakyan/hotelmanagement/repository/EmployeeRepository.java
+++ src/main/java/com/naltakyan/hotelmanagement/repository/EmployeeRepository.java
@@ -2,5 +2,7 @@ package com.naltakyan.hotelmanagement.repository;
 
 import com.naltakyan.hotelmanagement.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Long, Employee> {}
