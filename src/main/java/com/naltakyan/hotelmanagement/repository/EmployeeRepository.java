71f30f437517c4e9db27213aec7c269b56798dd8
Naltakyan <nareknaltakyan1@gmail.com>
2022-05-31 23:50:56 +0400

    fixes

--- src/main/java/com/naltakyan/hotelmanagement/repository/EmployeeRepository.java
+++ src/main/java/com/naltakyan/hotelmanagement/repository/EmployeeRepository.java
@@ -1,8 +1,15 @@
 package com.naltakyan.hotelmanagement.repository;
 
 import com.naltakyan.hotelmanagement.model.Employee;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+  Optional<Employee> findByIdAndDeletedIsNull(Long id);
+
+  Page<Employee> getEmployeesByDeletedIsNull(Pageable pageable);
+}
--- src/main/java/com/naltakyan/hotelmanagement/repository/GuestRepository.java
+++ src/main/java/com/naltakyan/hotelmanagement/repository/GuestRepository.java
@@ -1,16 +1,15 @@
 package com.naltakyan.hotelmanagement.repository;
 
 import com.naltakyan.hotelmanagement.model.Guest;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface GuestRepository extends JpaRepository<Guest, Long> {
-    Optional<Guest> findByIdAndDeletedIsNull(Long id);
+  Optional<Guest> findByIdAndDeletedIsNull(Long id);
 
-    Page<Guest> getGuestsByDeletedIsNull(Pageable pageable);
+  Page<Guest> getGuestsByDeletedIsNull(Pageable pageable);
 }
