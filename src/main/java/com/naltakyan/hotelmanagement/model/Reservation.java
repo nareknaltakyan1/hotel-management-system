ac5793dd26c5baa2a2a652c9734f37f90c4acdad
ArmanNaltakyan <92972034+ArmanNaltakyan@users.noreply.github.com>
2022-05-31 18:52:28 +0400

    edit status

--- src/main/java/com/naltakyan/hotelmanagement/model/Reservation.java
+++ src/main/java/com/naltakyan/hotelmanagement/model/Reservation.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class Reservation extends TimeStampedDomainEntity{
 
     @Enumerated(EnumType.STRING)
-    private ReservationStatus type;
+    private ReservationStatus status;
 
     @Column(name = "from_date")
     private LocalDateTime fromDate;
